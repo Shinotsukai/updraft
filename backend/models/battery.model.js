7d3cec0d31d4b17a3707030796aa8157950d289c
Alex <imagi.soft@gmail.com>
2019-08-25 16:41:35 +0100

    schema

--- backend/models/battery.model.js
+++ backend/models/battery.model.js
@@ -14,6 +14,6 @@ const batterySchema = new Schema ({
     timestamps: true,
 });
 
-const BatteryInfo = mongoose.model('BatteryInfo' batterySchema);
+const BatteryInfo = mongoose.model('BatteryInfo', batterySchema);
 
 module.exports = BatteryInfo;
